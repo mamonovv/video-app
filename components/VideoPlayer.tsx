4fd62efee8da99c6edcc464d581c83b6290383ff
mamonovv <mmvvlad@gmail.com>
2022-12-18 22:49:40 +0700

    add styles to VideoPlayer

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,13 +1,34 @@
+import {useRouter} from "next/router";
+import React from "react";
+
 function VideoPlayer({id}: { id: string }) {
+    const router = useRouter()
+
+    const handlePressBack = () => {
+        router.back()
+    }
+
     return (
-        <video
-            src={`/api/videos?videoId=${id}`}
-            width={"800px"}
-            height={"auto"}
-            controls
-            autoPlay
-            id={"video-player"}
-        />
+        <div
+            className={'w-screen h-screen bg-slate-500 flex flex-col gap-4 items-center justify-center text-fuchsia-50'}>
+            <h1 className={'text-2xl font-bold'}>Enjoy video!</h1>
+            <div>
+                <video
+                    src={`/api/videos?videoId=${id}`}
+                    width={"800px"}
+                    height={"auto"}
+                    controls
+                    id={"video-player"}
+                    className={'rounded-xl'}
+                />
+            </div>
+            <button onClick={handlePressBack}
+                    className={'uppercase rounded-lg cursor-pointer p-4 bg-gray-50 dark:hover:bg-bray-800 ' +
+                        'dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 ' +
+                        'dark:hover:bg-gray-600 mt-4'}>
+                Go back to upload page
+            </button>
+        </div>
     )
 }
 
